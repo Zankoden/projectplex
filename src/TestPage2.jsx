7346c65c2e9db4891413ea60e51eb7584044e7a9
Zankoden <zankodits@gmail.com>
2024-10-26 22:22:38 +0545

    commit save all good

--- src/TestPage2.jsx
+++ src/TestPage2.jsx
@@ -4,8 +4,13 @@ function TestPage2(){
     return(
         <>
         <h2>You are now at test page 2!!</h2>
+
         <Card cardName="Card 1"></Card>
         <Card cardName="Card 2"></Card>
+        <Card cardName="Card 3"></Card>
+        <Card cardName="Card 4"></Card>
+        <Card cardName="Card 5"></Card>
+
         </>
     )
 }
