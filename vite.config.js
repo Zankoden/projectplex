43eb4834704b372066fa41bccc6147c4bcbe7fcb
Zankoden <zankodits@gmail.com>
2024-10-27 10:41:59 +0545

    added in "vite.config.js"

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/projectplex/',  // Add this line to set the base path
+    build: {
+        outDir: 'dist'
+    }
 })
