d56cca9ca253d0a00b8e92d1ca93f683d2a87b6e
Zankoden <zankodits@gmail.com>
2024-10-27 12:44:29 +0545

    replaced "BrowserRouter" with "HashRouter"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Button from './TestButton'
 import TestPage from './TestPage'
 import TestPage2 from './TestPage2'
