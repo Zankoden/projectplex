7738d979bc8bfd95c93310212f08734412ff57d0
Zankoden <zankodits@gmail.com>
2024-10-27 11:01:21 +0545

    test vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/projectplex/',  // Add this line to set the base path
+  base: 'https://zankoden.github.io/projectplex',  // Add this line to set the base path
     build: {
         outDir: 'dist'
     },
