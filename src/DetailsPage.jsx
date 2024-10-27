af4bfb4d97f4c47cb6205cf390b8161e3dd66881
Zankoden <zankodits@gmail.com>
2024-10-27 09:48:39 +0545

    made details page design and json file for card contents

--- src/DetailsPage.jsx
+++ src/DetailsPage.jsx
@@ -1,9 +1,25 @@
+import Card from "./Card"
 
 function DetailsPage() {
 
     return (
         <>
             <h2>This is details page!</h2>
+            <div className="main-container">
+                <div className="main-card">
+                <p>Main Content</p>
+                    <Card /> {/* Main Card at the top */}
+                    
+                </div>
+                <div className="divider"></div>
+                <div className="sidebar-column">
+                    <p>Other Contents</p>
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </div>
         </>
     )
 }
