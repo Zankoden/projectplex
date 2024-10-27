e95d6e1e1a180ba8fd3da7c1014f8bd9171dc533
Zankoden <zankodits@gmail.com>
2024-10-27 10:46:34 +0545

    again changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/projectplex/',  // Add this line to set the base path
     build: {
         outDir: 'dist'
-    }
+    },
+    
+  plugins: [react()],
+  
 })
